4d1266321e4faece75ab10d11a119e0db169ce98
Dhaval Kotak <DhavalKotak@users.noreply.github.com>
2020-08-24 15:36:53 +0530

    Updates

---
+++ precache-manifest.ed5ff0bad14c64236b37199fb80484b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1691e2af8d5a0944f66a4b1e11a4baec",
+    "url": "/reddit-memeapi-app/index.html"
+  },
+  {
+    "revision": "65ce9373ea10248a636c",
+    "url": "/reddit-memeapi-app/static/css/main.d9298a2a.chunk.css"
+  },
+  {
+    "revision": "ecef22877e629b950dc3",
+    "url": "/reddit-memeapi-app/static/js/2.b0567e28.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reddit-memeapi-app/static/js/2.b0567e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ce9373ea10248a636c",
+    "url": "/reddit-memeapi-app/static/js/main.085a8d0f.chunk.js"
+  },
+  {
+    "revision": "9d890bf881e154af07db",
+    "url": "/reddit-memeapi-app/static/js/runtime-main.d13a84b5.js"
+  },
+  {
+    "revision": "bf06dcc35939237221da131fa6c05eee",
+    "url": "/reddit-memeapi-app/static/media/reddit.bf06dcc3.png"
+  }
+]);
